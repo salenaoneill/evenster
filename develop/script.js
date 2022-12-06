fdd38b0af5fccce1fc752cad1e278d059b2a7af4
Salena O'Neill <salenaoneill@salenas-air.lan>
2022-12-05 19:45:15 -0600

    retrieved event name and date from ticketmaster API

--- develop/script.js
+++ develop/script.js
@@ -1,24 +1,37 @@
 var ticketApiKey = "dJqLwkzfmRboei4GB7DDkGUtn5uG2Nk4";
-var userInput = "Minneapolis"
-var requestUrl = "https://app.ticketmaster.com/discovery/v2/events.json?keyword="+userInput+"&apikey=" + ticketApiKey ;
 var searchBtn = document.getElementById('city-searchBtn');
 var cityInput = document.getElementById('search');
 
 
 searchBtn.addEventListener('click', function(){
     var city = cityInput.value;
-    console.log(city);
+    getEvents(city);
 });
 
+function getEvents(city){
+    var requestUrl = "https://app.ticketmaster.com/discovery/v2/events.json?keyword="+ city +"&apikey=" + ticketApiKey ; 
+    $.ajax({
+        type: 'GET',
+        url: requestUrl, 
+        dataType: 'json',
+        success: function(response) {
+            console.log(response);
+            var eventName = response._embedded.events[0].name;
+            var eventDate = response._embedded.events[0].dates.start.localDate;
+            $("#Events").text(eventName);
+            $("#Dates").text(eventDate);
+        } 
+    })
+}
 
-fetch(requestUrl)
-.then(function (response){
-    console.log(response)
-    return response.json();
-})
-.then(function (data) {
-    console.log(data)
-  });
+// fetch(requestUrl)
+// .then(function (response){
+//     console.log(response)
+//     return response.json();
+// })
+// .then(function (data) {
+//     console.log(data)
+//   });
 
 
   // fetch('requestUrl')
